6de744deda3c087ed4ce9d0f20ad900858929e30
Ilan Brachmans <ilan.brachmans@gmail.com>
2021-07-17 17:57:12 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('.email-form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+
+    alert("Cadastrado com Sucesso!")
+
+})
